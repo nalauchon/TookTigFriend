e433141a7859fa6b319d75f15ccce0c70ee5746e
nalauchon <nalauchon@h-tech.ac.th>
2019-02-24 12:00:20 +0700

    update 24/02/2019 12:00PM

--- app/src/main/java/nalauchon/in/tooktigfriend/RegisterFragment.java
+++ app/src/main/java/nalauchon/in/tooktigfriend/RegisterFragment.java
@@ -1,13 +1,22 @@
 package nalauchon.in.tooktigfriend;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 
 /**
@@ -15,6 +24,10 @@ import android.view.ViewGroup;
  */
 public class RegisterFragment extends Fragment {
 
+//Explict
+    private boolean aBoolean = true;
+    private ImageView imageView;
+    private Uri uri;
 
     public RegisterFragment() {
         // Required empty public constructor
@@ -25,6 +38,95 @@ public class RegisterFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
 //        Create Toolbar
+        createToolbar();
+
+        //        Choose Image
+        chooseImage();
+
+    }   // Main Method
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+
+            uri = data.getData();
+            aBoolean = false;
+
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap, 800, 600, false);
+                imageView.setImageBitmap(bitmap1);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }   //if
+
+
+
+    }
+
+    private void chooseImage() {
+        imageView = getView().findViewById(R.id.imvAvata);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Please Choose App"),1);
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId()== R.id.itemUploand) {
+
+            checkValue();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void checkValue() {
+
+        MyAlert myAlert = new MyAlert(getActivity());
+
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText userEditText = getView().findViewById(R.id.edtUser);
+        EditText passwordEditText = getView().findViewById(R.id.edtPasswored);
+
+        String name = nameEditText.getText().toString().trim();
+        String user = userEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        if (aBoolean){
+//            Non Coose Image
+            myAlert.normalDialog("Non Choose Image", "Please Choose Avata");
+        } else if (name.isEmpty() || user.isEmpty() || password.isEmpty()) {
+//            Have Space
+            myAlert.normalDialog("Have Space", "Please Fill All Blank");
+        } else {
+
+        }
+
+    } //checkValue
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_register, menu);
+
+    }
+
+    private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
         ((MainActivity)getActivity()).setSupportActionBar(toolbar);
         ((MainActivity)getActivity()).getSupportActionBar().setTitle("Register");
@@ -36,10 +138,8 @@ public class RegisterFragment extends Fragment {
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
-
-
-
-    }   // Main Method
+        setHasOptionsMenu(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
