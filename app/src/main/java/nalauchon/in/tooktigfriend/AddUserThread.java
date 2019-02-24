864f3cc55204ddd8be61a5ef994db7e52b5cffbd
nalauchon <nalauchon@h-tech.ac.th>
2019-02-24 15:27:53 +0700

    update 24/02/2019 2:27PM

---
+++ app/src/main/java/nalauchon/in/tooktigfriend/AddUserThread.java
@@ -0,0 +1,48 @@
+package nalauchon.in.tooktigfriend;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+public class AddUserThread extends AsyncTask<String, Void, String> {
+
+    private Context context;
+
+    public AddUserThread(Context context){
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+
+        try {
+
+            OkHttpClient okHttpClient = new OkHttpClient();
+            RequestBody requestBody = new FormEncodingBuilder()
+                    .add( "isAdd", "true")
+                    .add("Name", strings[0])
+                    .add("User", strings[1])
+                    .add("Password",strings[2])
+                    .add("Avatar",strings[3])
+                    .build();
+            Request.Builder builder = new Request.Builder();
+            Request request = builder.url("http://www.androidthai.in.th/ksu/addUserNalauchon.php")
+                    .patch(requestBody).build();
+            Response response = okHttpClient.newCall(request).execute();
+            return response.body().string();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
+
+    }
+}
